d77fe250ce75bc32a1a79758053d2e29781feb50
Bo de Groot <bclm.degroot@gmail.com>
2024-01-26 17:43:41 +0100

    Add a email link for the form

--- assets/js/github.js
+++ assets/js/github.js
@@ -5,19 +5,19 @@ function userInformationHTML(user) {
                 (@<a href="${user.html_url}" target="_blank">${user.login}</a>)
             </span>
         </h2>
-        <div class="gh-content">
-            <div class="gh-avatar">
+        <div>
+            <div>
                 <a href="${user.html_url}" target="_blank">
-                    <img src="${user.avatar_url}" width="80" height="80" alt="${user.login}" />
+                    <img class="git-image" src="${user.avatar_url}" width="80" height="80" alt="${user.login}" />
                 </a>
             </div>
-            <p>Followers: ${user.followers} - Following ${user.following} <br> Repos: ${user.public_repos}</p>
+            <p class="git-followers">Followers: ${user.followers} - Following ${user.following} <br> Repos: ${user.public_repos}</p>
         </div>`;
 }
 
 function repoInformationHTML(repos) {
     if (repos.length == 0) {
-        return `<div class="clearfix repo-list">No repos!</div>`;
+        return `<div class="repo-list">No repos!</div>`;
     }
 
     var listItemsHTML = repos.map(function (repo) {
@@ -26,7 +26,7 @@ function repoInformationHTML(repos) {
                 </li>`;
     });
 
-    return `<div class="clearfix repo-list">
+    return `<div class="repo-list">
                 <p>
                     <strong>Repo List:</strong>
                 </p>
